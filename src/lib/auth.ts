5fdc892b46c48b1bcdc454fb88cfc811b1f769db
AjBernababe <andreijoshuabernabe@gmail.com>
2025-05-27 06:22:36 +0800

    reset reset

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../../prisma/prisma";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
-import dbConnect from "../db/dbConnect";
-import User from "@/models/User";
-import { loginSchema } from "./zodSchema";
 import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google,
     Credentials({
@@ -15,38 +15,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        try {
-          await dbConnect();
-
-          const validatedCredentials = loginSchema.parse(credentials);
-
-          const user = await User.findOne({
-            email: validatedCredentials.email,
-          });
-
-          if (!user) {
-            console.log("No user found with the provided email.");
-            return null;
-          }
-
-          const isValidPassword = await bcrypt.compare(
-            validatedCredentials.password,
-            user.password
-          );
-
-          if (!isValidPassword) {
-            console.log("Invalid password.");
-            return null;
-          }
-
-          return {
-            id: user._id.toString(),
-            name: user.name,
-          };
-        } catch (error) {
-          console.log("Error during authorization:", error);
-          return null;
-        }
+        console.log(credentials);
       },
     }),
   ],
