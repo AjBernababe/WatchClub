da6c69507139c33a8d866ea88a04a920f394bc00
AjBernababe <andreijoshuabernabe@gmail.com>
2025-07-14 13:29:33 +0800

    suspense

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,11 +1,12 @@
 import { SearchBar } from "@/components/explorePage/searchBar";
 import { TmdbExplore } from "@/components/explorePage/tmdbExplore";
+import { Suspense } from "react";
 
 export default function ExplorePage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading…</div>}>
       <SearchBar />
       <TmdbExplore />
-    </>
+    </Suspense>
   );
 }
