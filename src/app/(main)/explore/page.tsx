33f1b5b2c3001e4adabd57f333f99095373360c8
AjBernababe <andreijoshuabernabe@gmail.com>
2025-08-26 19:15:43 +0800

    reverted stuffs to relearn

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,12 +1,3 @@
-import { SearchBar } from "@/components/explorePage/searchBar";
-import { TmdbExplore } from "@/components/explorePage/tmdbExplore";
-import { Suspense } from "react";
-
-export default function ExplorePage() {
-  return (
-    <Suspense fallback={<div>Loading…</div>}>
-      <SearchBar />
-      <TmdbExplore />
-    </Suspense>
-  );
+export default function EplorePage() {
+  return <h1>Explore</h1>;
 }
